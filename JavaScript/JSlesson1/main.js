0fd99da5321de44e6d4246c0867c3a5afa79d143
Maksym Danylko <maksymdanylko23@gmail.com>
2024-03-14 22:52:00 +0200

    виконав рахунок кофе

---
+++ JavaScript/JSlesson1/main.js
@@ -0,0 +1,8 @@
+{
+  let forMyCoffe = 10;
+  let forGuestsCoffe = 20;
+  let somePartialResult = forMyCoffe * 4;
+  let someOtherPartialResult = forGuestsCoffe / 6;
+  let result = somePartialResult + someOtherPartialResult;
+  alert(result);
+};
